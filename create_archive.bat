d5ebc07e5d248461468517a92a71bd1b1ac45a6f
Andreas G. <13819164+agaertner@users.noreply.github.com>
2024-08-18 22:13:16 +0200

    ignore archive and gitignore

--- create_archive.bat
+++ create_archive.bat
@@ -3,7 +3,7 @@ setlocal
 set zipname=temp.zip
 set finalname=Nekres.Markers.taco
 set scriptname=%~nx0
-powershell -command "$items = Get-ChildItem | Where-Object { $_.FullName -ne '%~dpnx0' }; Compress-Archive -Path $items.FullName -DestinationPath '%zipname%' -Force"
+powershell -command "$items = Get-ChildItem | Where-Object { $_.FullName -ne '%~dpnx0' -and $_.Name -ne '.gitignore' -and $_.Name -ne '%finalname%' -and $_.Name -ne '%zipname%'}; Compress-Archive -Path $items.FullName -DestinationPath '%zipname%' -Force"
 if exist %finalname% del %finalname%
 ren %zipname% %finalname%
 echo SUCCESS! Marker Pack created: '%finalname%'
